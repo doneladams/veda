66930eefd459e50ff502548f89e1f9593d11322b
itiu <ValeriyBushenev@gmail.com>
2017-01-17 13:32:31 +0300

    *core: *controll-install.sh: * tarantool-c dependency

--- control-install.sh
+++ control-install.sh
@@ -181,6 +181,30 @@ if ! ldconfig -p | grep libraptor2; then
 
 fi
 
+if ! tarantool -V | grep $TARANTOOL_VER; then
+
+curl http://download.tarantool.org/tarantool/1.7/gpgkey | sudo apt-key add -
+release=`lsb_release -c -s`
+
+# install https download transport for APT
+sudo apt-get -y install apt-transport-https
+
+# append two lines to a list of source repositories
+sudo rm -f /etc/apt/sources.list.d/*tarantool*.list
+sudo tee /etc/apt/sources.list.d/tarantool_1_7.list <<- EOF
+deb http://download.tarantool.org/tarantool/1.7/ubuntu/ $release main
+deb-src http://download.tarantool.org/tarantool/1.7/ubuntu/ $release main
+EOF
+
+# install
+sudo apt-get update
+sudo apt-get -y install tarantool
+#sudo apt-get -y install libtarantool-dev
+
+tarantool -V
+
+fi
+
 if ! ldconfig -p | grep libtarantool; then
 
     mkdir tmp
@@ -205,28 +229,4 @@ if ! ldconfig -p | grep libtarantool; then
     cd ..
     cd ..
     
-fi
-
-if ! tarantool -V | grep $TARANTOOL_VER; then
-
-curl http://download.tarantool.org/tarantool/1.7/gpgkey | sudo apt-key add -
-release=`lsb_release -c -s`
-
-# install https download transport for APT
-sudo apt-get -y install apt-transport-https
-
-# append two lines to a list of source repositories
-sudo rm -f /etc/apt/sources.list.d/*tarantool*.list
-sudo tee /etc/apt/sources.list.d/tarantool_1_7.list <<- EOF
-deb http://download.tarantool.org/tarantool/1.7/ubuntu/ $release main
-deb-src http://download.tarantool.org/tarantool/1.7/ubuntu/ $release main
-EOF
-
-# install
-sudo apt-get update
-sudo apt-get -y install tarantool
-sudo apt-get -y install libtarantool-dev
-
-tarantool -V
-
 fi
