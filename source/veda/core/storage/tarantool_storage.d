10d0ba3c6a6b70088d8b2d129a0aa543f7926bbf
Itiu <ValeriyBushenev@gmail.com>
2017-01-23 21:54:04 +0300

    ++ inital tarantool storage interface

---
+++ source/veda/core/storage/tarantool_storage.d
@@ -0,0 +1,44 @@
+/**
+ * реализация хранилища, используя tarantool
+ */
+module veda.core.storage.tarantool_storage;
+
+import veda.core.common.context;
+
+public class LmdbStorage : Storage
+{
+    public ResultCode put(string in_key, string in_value, long op_id)
+    {
+    	throw new Exception ("not implemented");
+    }
+    
+    public string find(string uri, bool return_value = true)
+    {
+    	throw new Exception ("not implemented");    	
+    }
+    
+    public int get_of_cursor(bool delegate(string key, string value) prepare)
+    {
+    	throw new Exception ("not implemented");    	    
+    }
+    
+    public long count_entries()
+    {
+    	throw new Exception ("not implemented");    	
+    }
+    
+    public void reopen_db()
+    {
+    	throw new Exception ("not implemented");    	
+    }
+    
+    public void close_db()
+    {
+    	throw new Exception ("not implemented");    	
+    }
+    
+    public long dump_to_binlog()
+    {
+    	throw new Exception ("not implemented");    	
+    }	
+}
