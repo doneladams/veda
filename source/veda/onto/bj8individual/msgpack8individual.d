7898efeece2d17eaeb1e89bb7ca82f788c9818f6
Itiu <ValeriyBushenev@gmail.com>
2017-02-01 23:07:27 +0300

    *core: individual <-> msgpack

--- source/veda/onto/bj8individual/msgpack8individual.d
+++ source/veda/onto/bj8individual/msgpack8individual.d
@@ -93,7 +93,7 @@ private char *write_resources(string uri, ref Resources vv, char *w)
                 w = mp_encode_array(w, 2);
                 w = mp_encode_uint(w, DataType.String);
                 w = mp_encode_str(w, cast(char *)svalue, cast(uint)svalue.length);
-            }    
+            }
         }
     }
     return w;
@@ -115,17 +115,100 @@ public int msgpack2individual(Individual *individual, string in_str)
 {
     try
     {
-    	char *ptr = cast(char*)in_str.ptr;
-    	int size = mp_decode_array(&ptr);
-    	
-    	if (size != 2)
-	    	return -1;
-	    	
-	    uint uri_lenght; 	
-	    char *uri = mp_decode_str(&ptr, &uri_lenght);
-	
-    	
-        return -1;//read_element(individual, cast(ubyte[])in_str, dummy);
+        char *ptr         = cast(char *)in_str.ptr;
+        int  root_el_size = mp_decode_array(&ptr);
+
+        if (root_el_size != 2)
+            return -1;
+
+        uint uri_lenght;
+        char *uri = mp_decode_str(&ptr, &uri_lenght);
+        individual.uri = uri[ 0..uri_lenght ].dup;
+
+        int predicates_length = mp_decode_map(&ptr);
+
+        foreach (idx; 0..predicates_length)
+        {
+            uint      key_lenght;
+            char      *key      = mp_decode_str(&ptr, &key_lenght);
+            string    predicate = key[ 0..key_lenght ].dup;
+
+            Resources resources = Resources.init;
+
+            mp_type   el_type = mp_typeof(*ptr);
+
+            if (el_type == mp_type.MP_ARRAY)
+            {
+                int predicate_el_length = mp_decode_array(&ptr);
+                if (predicate_el_length == 2)
+                {
+                    long type = mp_decode_uint(&ptr);
+
+                    if (type == DataType.Datetime)
+                    {
+                        long value = mp_decode_uint(&ptr);
+                        resources ~= Resource(DataType.Datetime, value);
+                    }
+                    else if (type == DataType.String)
+                    {
+                        uint val_length;
+                        char *val = mp_decode_str(&ptr, &val_length);
+                        resources ~= Resource(DataType.String, val[ 0..val_length ].dup);
+                    }
+                    else
+                        return -1;
+                }
+                else if (predicate_el_length == 3)
+                {
+                    long type = mp_decode_uint(&ptr);
+
+                    if (type == DataType.Decimal)
+                    {
+                        long mantissa = mp_decode_uint(&ptr);
+                        long exponent = mp_decode_uint(&ptr);
+                        resources ~= Resource(decimal(mantissa, cast(byte)exponent));
+                    }
+                    else if (type == DataType.String)
+                    {
+                        uint val_length;
+                        char *val = mp_decode_str(&ptr, &val_length);
+                        long lang = mp_decode_uint(&ptr);
+                        resources ~= Resource(DataType.String, val[ 0..val_length ].dup, cast(LANG)lang);
+                    }
+                    else
+                        return -1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else if (el_type == mp_type.MP_STR)
+            {
+                // this uri
+                uint val_length;
+                char *val = mp_decode_str(&ptr, &val_length);
+                resources ~= Resource(DataType.Uri, val[ 0..val_length ].dup);
+            }
+            else if (el_type == mp_type.MP_INT)
+            {
+                // this int
+                long val = mp_decode_uint(&ptr);
+                resources ~= Resource(DataType.Integer, val);
+            }
+            else if (el_type == mp_type.MP_BOOL)
+            {
+                // this bool
+                long val = mp_decode_bool(&ptr);
+                resources ~= Resource(DataType.Boolean, val);
+            }
+            else
+                return -1;
+
+            individual.resources[ predicate ] = resources;
+        }
+
+        return -1; //read_element(individual, cast(ubyte[])in_str, dummy);
     }
     catch (Throwable ex)
     {
