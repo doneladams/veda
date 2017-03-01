59efd5fb73e508692d470da6e47d3b4a205b2c6a
itiu <ValeriyBushenev@gmail.com>
2017-03-01 14:16:43 +0300

    *

--- tarantool/compile_listener.sh
+++ tarantool/compile_listener.sh
@@ -1,6 +1,6 @@
 rm c_listener.so
 clang++ -Wall -L/usr/local/lib -lnanomsg -shared -o c_listener.so -fPIC c_listener.cpp -I/usr/include/lua5.3 
 #cp c_listener.so ~/TarantoolVeda/
-cp c_listener.so ../
+#cp c_listener.so ../
 #cp listener.lua ~/TarantoolVeda/
-cp listener.lua ../
+#cp listener.lua ../
