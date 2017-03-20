a2f8cb2905dc5e1ef1193c55fbd5cc753d04d4d0
Itiu <ValeriyBushenev@gmail.com>
2017-03-20 18:11:17 +0300

    * ttstorage

--- source/veda/core/storage/tarantool_storage.d
+++ source/veda/core/storage/tarantool_storage.d
@@ -8,62 +8,68 @@ import veda.connector.connector;
 
 public class TarantoolStorage : Storage
 {
-	string host;
-	int port;
-    Logger              log;
-    	
-    this(string _host, int _port, Logger _log)
+    string host;
+    ushort port;
+    Logger log;
+
+    this(string _host, ushort _port, Logger _log)
     {
-    	host = _host;
-    	port = _port;
+        host = _host;
+        port = _port;
         log  = _log;
-    }	
-	
+    }
+
     public ResultCode put(string in_key, string in_value, long op_id)
     {
-    	log.trace ("ERR! put not implemented");
-    	return ResultCode.Fail_Store;
-    	//throw new Exception ("not implemented");
+        RequestResponse rr = Connector.put(host, port, false, null, [ in_value ]);
+
+        if (rr !is null)
+            return rr.common_rc;
+
+        return ResultCode.Fail_Store;
     }
-    
+
     public string find(string uri, bool return_value = true)
     {
-    	log.trace ("ERR! find not implemented");
-    	return "";
+        RequestResponse rr = Connector.get(host, port, false, null, [ uri ]);
+
+        if (rr !is null && rr.msgpacks.length > 0)
+            return rr.msgpacks[ 0 ];
+
+        return null;
     }
-    
+
     public int get_of_cursor(bool delegate(string key, string value) prepare)
     {
-    	log.trace ("ERR! get_of_cursor not implemented");
-    	throw new Exception ("not implemented");    	    
+        log.trace("ERR! get_of_cursor not implemented");
+        throw new Exception("not implemented");
     }
-    
+
     public long count_entries()
     {
-    	log.trace ("ERR! count_entries not implemented");
-    	throw new Exception ("not implemented");    	
+        log.trace("ERR! count_entries not implemented");
+        throw new Exception("not implemented");
     }
-    
+
     public void reopen_db()
     {
-    	//throw new Exception ("not implemented");    	
+        //throw new Exception ("not implemented");
     }
-    
+
     public void close_db()
     {
-    	//throw new Exception ("not implemented");    	
+        //throw new Exception ("not implemented");
     }
-    
+
     public long dump_to_binlog()
     {
-    	log.trace ("ERR! dump_to_binlog not implemented");
-    	throw new Exception ("not implemented");    	
-    }	
+        log.trace("ERR! dump_to_binlog not implemented");
+        throw new Exception("not implemented");
+    }
 
     public ResultCode remove(string in_key)
     {
-    	log.trace ("ERR! remove not implemented");
-    	throw new Exception ("not implemented");    	
-    }	
-
+        log.trace("ERR! remove not implemented");
+        throw new Exception("not implemented");
+    }
 }
