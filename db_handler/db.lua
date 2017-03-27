6b9e3f3a905ab2ed886140228ece6266d7b1df9d
Itiu <ValeriyBushenev@gmail.com>
2017-03-27 20:06:32 +0300

    *core: sp (10)

--- db_handler/db.lua
+++ db_handler/db.lua
@@ -8,22 +8,22 @@ socket.tcp_server('0.0.0.0', 3303, function(s)
         for i=1, 4, 1 do
             size = bit.lshift(size, 8) + string.byte(s:read(1))
         end
-        print('size='..size)
+        --print('size='..size)
         
         op = string.byte(s:read(1))
-        print('op='..op)
+        --print('op='..op)
         
         msg = s:read(size)
-        print('msg='..msg)
+        --print('msg='..msg)
         resp = db_handle_request(op, msg)
         resp_size = string.len(resp)
-        print('resp_len='..resp_size)
-        print('resp='..resp)
+        --print('resp_len='..resp_size)
+        --print('resp='..resp)
         resp_size_str = string.char(bit.band(bit.rshift(resp_size, 24), 255)) ..
             string.char(bit.band(bit.rshift(resp_size, 16), 255)) ..
             string.char(bit.band(bit.rshift(resp_size, 8), 255)) ..
             string.char(bit.band(resp_size, 255))
-        print('resp_size_str='..resp_size_str)
+        --print('resp_size_str='..resp_size_str)
         s:send(resp_size_str)
         s:send(resp)
 
