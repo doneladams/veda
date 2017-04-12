79c96dd323548ce8e20b25e698a58b53417b0856
Itiu <ValeriyBushenev@gmail.com>
2017-04-12 16:04:15 +0300

    *core: sp (34)

--- db_handler/init_tarantool.lua
+++ db_handler/init_tarantool.lua
@@ -1,4 +1,4 @@
-box.cfg{listen=3309, work_dir='./data/tarantool', slab_alloc_arena=12.0, slab_alloc_maximal=67108864, log_level=5, logger='./tarantool.log'}
+box.cfg{listen=3309, work_dir='./data/tarantool', log_level=5, logger='./tarantool.log', memtx_memory=268435456.0}
 log = require('log')
 
 if box.space.individuals == nil then
