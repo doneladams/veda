604d914a6c3f6cfdb7436e734fede1906529e417
Vadim <muller95@yandex.ru>
2017-04-19 14:04:57 +0300

    Added dummy for requests, gitignore for cargo targer

--- rust_db_handler/db_handler/src/db_handler.rs
+++ rust_db_handler/db_handler/src/db_handler.rs
@@ -3,9 +3,8 @@ extern crate rmp_bind;
 
 mod rest;
 
-use std::io::{ Write, stderr };
+use std::io::{ Cursor, Write, stderr };
 use rmp_bind::{ decode, encode };
-
 // use rest::{ Codes, put };
 // use rest::rmp_bind;
 
@@ -20,47 +19,57 @@ pub struct Response {
     size: usize
 }
 
+fn fail(resp_msg: &mut Vec<u8>, code: rest::Codes, err_msg: String) {
+    writeln!(&mut stderr(), "{0}", err_msg).unwrap();
+    encode::encode_array(resp_msg, 1);
+    encode::encode_uint(resp_msg, code as u64);
+}
 
-fn unmarshal_request(mut msg: &[u8], arr_size: u64, resp_msg: &mut Vec<u8>) {
+
+fn unmarshal_request(cursor: &mut Cursor<&[u8]>, arr_size: u64, resp_msg: &mut Vec<u8>) {
     if arr_size < 4 {
-        writeln!(&mut stderr(), "@INVALID MSGPACK SIZE < 4").unwrap();
+        fail(resp_msg, rest::Codes::BadRequest, format!("{}", "@INVALID MSGPACK SIZE < 4"));
         return;
     }
-
-
 
     writeln!(&mut stderr(), "@UNMARSHAL").unwrap();
     encode::encode_array(resp_msg, 1);
     encode::encode_uint(resp_msg, rest::Codes::InternalServerError as u64);
-    /*let op = read_int(&mut &msg[..]).unwrap();
-    writeln!(&mut stderr(), "@op code {0}", op).unwrap();
-    let need_auth = read_bool(&mut &msg[..]).unwrap();
-    writeln!(&mut stderr(), "@op code {0}", need_auth).unwrap();    
-    match op {
-        PUT => put(msg, arr_size, need_auth, resp_msg),
-        GET => println!("GET"),
-        AUTHORIZE => println!("AUTH"),
-        REMOVE => println!("REMOVE"),
-        _ => fail(resp_msg, format!("@ERR UNKNOWN REQUEST {0}", op))
-    }*/
-}
-
-fn fail(resp_msg: &mut Vec<u8>, err_msg: String) {
-/*    writeln!(&mut stderr(), "{0}", err_msg).unwrap();
-    write_array_len(resp_msg, 1).unwrap();
-    write_u64(resp_msg, Codes::InternalServerError as u64).unwrap();*/
+    let mut op_code: u64 = 0;
+    match decode::decode_uint(cursor) {
+        Err(err) => return fail(resp_msg, rest::Codes::BadRequest, err),
+        Ok(op) => (op_code = op)
+    }
+    writeln!(&mut stderr(), "@op code {0}", op_code).unwrap();
+    let mut need_auth: bool = false;
+    writeln!(&mut stderr(), "@need_auth {0}", need_auth).unwrap();    
+    match decode::decode_bool(cursor) {
+        Err(err) => return fail(resp_msg, rest::Codes::BadRequest, err),
+        Ok(v) => (need_auth = v)
+    }
+    match op_code {
+        PUT => writeln!(&mut stderr(), "PUT").unwrap(),
+        GET => writeln!(&mut stderr(), "GET").unwrap(),
+        AUTHORIZE => writeln!(&mut stderr(), "AUTH").unwrap(),
+        REMOVE => writeln!(&mut stderr(), "REMOVE").unwrap(),
+        _ => fail(resp_msg, rest::Codes::BadRequest, format!("@ERR UNKNOWN REQUEST {0}", op_code))
+    }
 }
 
 #[no_mangle]
 pub extern fn handle_request(mut msg: &[u8]) -> Response {
     unsafe {
-        let result = decode::decode_array(msg);
+        let mut cursor = Cursor::new(msg);
+       // let result = decode::decode_array(&mut cursor);
         let resp_msg = &mut Vec::new();
-         match  result {
-            Err(err) => fail(resp_msg, err),
-            Ok(arr_size) => unmarshal_request(msg, arr_size, resp_msg)
+        let mut arr_size: u64 = 0;
+        match  decode::decode_array(&mut cursor) {
+            Err(err) => fail(resp_msg, rest::Codes::InternalServerError, err),
+            Ok(arr_size) => unmarshal_request(&mut cursor, arr_size, resp_msg)
         }
-        writeln!(&mut stderr(), "ENDING");
+        
+
+        writeln!(&mut stderr(), "ENDING").unwrap();
         return Response { msg: &resp_msg[0], size: resp_msg.len() } ;
     }
 }
--- rust_db_handler/rmp_bind/src/decode.rs
+++ rust_db_handler/rmp_bind/src/decode.rs
@@ -1,21 +1,70 @@
 extern crate rmp;
 
+use std::io::Cursor;
 use std::result;
+use std::fmt;
 use self::rmp::decode;
+use std::io::{Write, stderr};
 
-/*pub fn read_uint(mut msg: &[u8]) -> u64 {
-    let val: u64 = 0;
-    match rmp::decode::read_marker(mut msg: &[u8]) {
-        rmp::Marker::U8 => println!("u8"),
-        rmp::Marker::U16 => println!("u16"),
-        rmp::Marker::U32 => println!("u32"),
-        rmp::Marker::U64 => println!("u64")
+fn decode_uint8(cursor: &mut Cursor<&[u8]>) -> u64 {
+    return decode::read_u8(cursor).unwrap() as u64;
+}
+
+fn decode_uint16(cursor: &mut Cursor<&[u8]>) -> u64 {
+    // writeln!(&mut stderr(), "marker = {:?}", marker); 
+    return decode::read_u16(cursor).unwrap() as u64;
+}
+
+fn decode_uint32(cursor: &mut Cursor<&[u8]>) -> u64 {
+    return decode::read_u32(cursor).unwrap() as u64;
+}
+
+fn decode_uint64(cursor: &mut Cursor<&[u8]>) -> u64 {
+    return decode::read_u64(cursor).unwrap();
+}
+
+fn decode_pfix(cursor: &mut Cursor<&[u8]>, size: u8) -> u64 {
+    let mut val: u64 = 0;
+    writeln!(stderr(), "DECODE PFIX {0}", size);
+    for i in 0 .. size {
+        writeln!(stderr(), "DECODE BYTE {0}", size);
+        let byte = decode::read_pfix(cursor).unwrap();
+        val <<= 8;
+        val |= byte as u64;
     }
-    return 0;
-}*/
+    return val;   
+}
 
-pub fn decode_array(mut buf: &[u8]) -> Result<u64, String> {
-    match decode::read_array_len(&mut &buf[..]) {
+pub fn decode_uint(cursor: &mut Cursor<&[u8]>) -> Result<u64, String> {
+    let val: u64 = 0;
+    let curr_position = cursor.position();
+    // return Ok(decode::read_pfix(cursor).unwrap() as u64);
+     
+    let marker = decode::read_marker(cursor).unwrap();
+    cursor.set_position(curr_position);
+    // writeln!(&mut stderr(), "marker = {:?}", marker); 
+    // rmp::Marker::FixPos();
+    match marker {
+        rmp::Marker::U8  => Ok(decode_uint8(cursor)),
+        rmp::Marker::U16  => Ok(decode_uint16(cursor)),
+        rmp::Marker::U32 => Ok(decode_uint32(cursor)),
+        rmp::Marker::U64  => Ok(decode_uint64(cursor)),
+        _ => match decode::read_pfix(cursor) {
+            Ok(v)  => Ok(v as u64),
+            Err(err) => Err(format!("@ERR IS NOT UINT {:?}", marker))
+        }
+    }
+}
+
+pub fn decode_bool(cursor: &mut Cursor<&[u8]>) -> Result<bool, String> {
+    match decode::read_bool(cursor) {
+        Ok(v) => Ok(v),
+        Err(err) => Err(format!("@ERR IS NOT BOOL"))
+    }
+}
+
+pub fn decode_array(cursor: &mut Cursor<&[u8]>) -> Result<u64, String> {
+    match decode::read_array_len(cursor) {
         Ok(arr_size) => Ok(arr_size as u64),
         Err(err) => Err(format!("@ERR DECODING ARRAY {0}", err))
     }
