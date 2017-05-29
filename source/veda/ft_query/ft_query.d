476a8a7649bb8d66632dd8a9381c7d56cdc1957c
Itiu <ValeriyBushenev@gmail.com>
2017-05-29 21:24:13 +0300

    * veda-ft-query

--- source/veda/ft_query/ft_query.d
+++ source/veda/ft_query/ft_query.d
@@ -1,8 +1,8 @@
 /**
  * filltext query module
  */
-import std.stdio, std.socket, std.conv;
-import core.thread; import core.atomic;
+import std.stdio, std.socket, std.conv, std.array, std.outbuffer;
+import core.thread, core.atomic;
 import veda.common.logger, veda.core.common.context, veda.core.impl.thread_context;
 
 class HandlerThread : Thread
@@ -21,32 +21,59 @@ public:
 private:
     void run()
     {
-        string request = _recv(socket);
+        SearchResult res;
+        string       request = _recv(socket);
 
-        // TODO: надо из request вытащить нижеуказанные параметры
-        string _ticket;
-        string query_str;
-        string sort_str;
-        string db_str;
-        int    from, top, limit;
-		//
-		
-        Ticket *ticket;
+        string[]     els = request.split('|');
+        if (els.length == 8)
+        {
+            string _ticket    = els[ 0 ];
+            string _query     = els[ 1 ];
+            string _sort      = els[ 2 ];
+            string _databases = els[ 3 ];
+            bool   _reopen    = to!bool(els[ 4 ]);
+            int    _top       = to!int (els[ 5 ]);
+            int    _limit     = to!int (els[ 6 ]);
+            int    _from      = to!int (els[ 7 ]);
+            //
 
-        ticket = context.get_ticket(_ticket);
+            Ticket *ticket;
+            ticket = context.get_ticket(_ticket);
 
-        SearchResult res = context.get_individuals_ids_via_query(ticket, query_str, sort_str, db_str, from, top, limit, null, false);
+            res = context.get_individuals_ids_via_query(ticket, _query, _sort, _databases, _from, _top, _limit, null, false);
+        }
 
-        // TODO: необходимо перед отправкой серелизовать [res]
-
-        string response = text(res);
-
+        string response = to_json_str(res);
         _send(socket, response);
 
         socket.close();
 
         ctx_pool.free_context(context);
     }
+}
+
+
+private string to_json_str(SearchResult res)
+{
+    OutBuffer bb = new OutBuffer();
+
+    bb.write("{\"result\":[");
+
+    foreach (idx, rr; res.result)
+    {
+        if (idx > 0)
+            bb.write(',');
+
+        bb.write(rr);
+
+        bb.write('"');
+        bb.write('"');
+    }
+
+    bb.writef("], \"count\":%d,\"estimated\":%d,\"processed\":%d,\"cursor\":%d,\"result_code\":%d}", res.count, res.estimated, res.processed,
+              res.cursor,
+              res.result_code);
+    return bb.toString();
 }
 
 private string _recv(Socket socket)
@@ -123,14 +150,14 @@ void main()
     log = new Logger("veda-core-ft-query", "log", "");
 
     ctx_pool = new ContextPool();
-	Context context = ctx_pool.allocate_context();
-	ctx_pool.free_context(context);
+    Context context = ctx_pool.allocate_context();
+    ctx_pool.free_context(context);
 
     while (true)
     {
-        Socket  socket  = listener.accept();
+        Socket socket = listener.accept();
         context = ctx_pool.allocate_context();
-        auto    ht      = new HandlerThread(socket, context);
+        auto   ht = new HandlerThread(socket, context);
         ht.start();
     }
 }
