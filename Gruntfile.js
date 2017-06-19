f73b742aabdd1ed16f30bbf0e045e5f195246a65
Itiu <ValeriyBushenev@gmail.com>
2017-06-19 23:26:13 +0300

    *qa

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,6 +83,7 @@ module.exports = function(grunt) {
 					urls : [ 'http://localhost:8080/tests?coverage=true&lcovReport' ],
 					noGlobals : true,
 					stack : true,
+					force : true,
 					timeout: 220000
 				}
 			}
