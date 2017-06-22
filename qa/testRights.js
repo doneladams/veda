7d31a643ceb9d73ef4626f2ba72fb4d036feb42e
Itiu <ValeriyBushenev@gmail.com>
2017-06-22 16:45:10 +0300

    *qa

--- qa/testRights.js
+++ qa/testRights.js
@@ -47,7 +47,7 @@ basic.getDrivers().forEach (function (drv) {
     //PHASE#1
     var driver = basic.getDriver(drv);
     basic.openPage(driver, drv);
-    basic.login(driver, 'bychinat', '123', '4', 'Администратор4');
+    basic.login(driver, 'bushenevvt', '123', '4', 'Администратор4');
     basic.logout(driver);
     basic.login(driver, 'karpovrt', '123', '2', 'Администратор2');
     driver.findElement({css:'#app'}).then(function(){console.log("****** PHASE#1 > LOGIN            : COMPLETE");});
