288605091398bfcf3e5200a3deab34284217c1c4
Itiu <ValeriyBushenev@gmail.com>
2017-07-02 22:50:42 +0300

    *qa

---
+++ send-logs-to-chunk-io.sh
@@ -0,0 +1,23 @@
+cd logs
+
+count=$(git ls-files -o | wc -l)
+
+git ls-files -o
+
+echo ">>>>>>>>> CONTAINERS LOG FILES <<<<<<<<<<<<"
+
+for (( i=1; i<"$count";i++ ))
+
+do
+
+file=$(echo $(git ls-files -o | sed "${i}q;d"))
+
+echo "$file"
+
+cat $file | curl -sT - chunk.io
+
+done
+
+echo " >>>>> testsummary log file <<<< "
+
+cat testsummary.log | curl -sT - chunk.io
