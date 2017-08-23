be3d1ceef29b7bc06f933bf0f9be09b3f0d0c4cb
Vadim <muller95@yandex.ru>
2017-08-23 22:32:30 +0300

    reading from queue

--- source/goindexer/main.go
+++ source/goindexer/main.go
@@ -1,11 +1,61 @@
 package main
 
 import (
-	"encoding/json"
+	"crypto/md5"
+	"database/sql"
 	"fmt"
 	"log"
 	"strings"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+var configCommon = `
+indexer
+{
+	mem_limit		= 1024M
+}
+
+searchd
+{
+	listen			= 9312
+	listen			= 9306:mysql41
+
+	log			= logs/searchd.log
+
+	query_log		= logs/searchd_query.log
+	read_timeout		= 5
+
+	client_timeout		= 300
+
+	max_children		= 30
+
+	persistent_connections_limit	= 30
+
+	pid_file		= data/searchd.pid
+
+	seamless_rotate		= 1
+
+	preopen_indexes		= 1
+
+	unlink_old		= 1
+
+	mva_updates_pool	= 1M
+
+	max_packet_size		= 8M
+
+	max_filters		= 256
+
+	max_filter_values	= 4096
+
+	max_batch_queries	= 32
+
+	workers			= threads
+
+	binlog_path = data/
+}
+`
 
 var conn Connector
 
@@ -24,91 +74,89 @@ func main() {
 
 	onto := NewOnto()
 	onto.Load()
-	/*
-		ResType             DataType
-		Lang                Lang
-		StrData             string
-		BoolData            bool
-		LongData            int64
-		DecimalMantissaData int64
-		DecimalExponentData int64
-	*/
-	/*	configCommon := `
-			rt_field = uri
-			rt_field = res_type
-			rt_filed = lang
-			rt_field = str_data
-			rt_field = bool_data
-			rt_field = long_data
-			rt_field = decimal_mantissa_data
-			rt_field = decimal_exponent_data
 
-			rt_attr_string = uri_attr
-			rt_attr_biging = res_type_attr
-			rt_attr_bigint = lang_attr
-			rt_attr_bool = bool_data_attr
-			rt_attr_bigint = long_data_attr
-			rt_attr_bigint = decimal_mantissa_data_attr
-			rt_attr_bigint = decimal_exponent_data_attr
-		}
-		`*/
+	createSphinxConfig(&onto)
 
-	request := fmt.Sprintf("%v�'rdfs:domain'==='%s'���false�0�10000�0", systicket.Id, "v-s:Person")
-	rc, queryBytes := query(request)
-	if rc != Ok {
-		log.Printf("@ERR ON QUERY RDFS:DOMAIN %v: %v\n", "v-s:Person", rc)
-	}
-
-	var jsonData map[string]interface{}
-	err := json.Unmarshal(queryBytes, &jsonData)
+	dbConn, err := sql.Open("mysql", "@tcp(127.0.0.1:9306)/db")
 	if err != nil {
-		log.Printf("@ERR ON DECODING QUERY RDFS:DOMAIN %v RESPONSE JSON: %v\n", "v-s:Person", err)
-		return
+		log.Fatal("@ERR CONNECTING TO SPHINX: ", err)
 	}
 
-	urisI := jsonData["result"].([]interface{})
-	uris := make([]string, len(urisI))
-	for i := 0; i < len(urisI); i++ {
-		uris[i] = urisI[i].(string)
-	}
+	main_queue_name := "individuals-flow"
+	var main_queue *Queue
+	var main_cs *Consumer
 
-	rr := conn.Get(false, "cfg:VedaSystem", uris, false)
+	main_queue = NewQueue(main_queue_name, R)
+	main_queue.open(CURRENT)
 
-	if rr.CommonRC != Ok {
-		log.Printf("@ERR COMMON ON GET RDFS:DOMAIN %v: %v\n", "v-s:Person", rr.CommonRC)
-		return
-	}
+	main_cs = NewConsumer(main_queue, "goindexer")
+	main_cs.open()
 
-	fmt.Printf("index %s {\n", "v_s_Person")
-	fmt.Printf("\ttype=rt\n")
-	fmt.Printf("\tpath=data/sphinx-indexes/%s\n", "v_s_Person")
-	fmt.Printf("\trt_field = uri\n\trt_attr_string=uri_attr\n")
-	for i := 0; i < len(rr.Data); i++ {
-		domain := MsgpackToIndividual(rr.Data[i])
-		rdfsRange := domain.Resources["rdfs:range"][0].StrData
-		attr := strings.Replace(strings.Replace(domain.Uri, ":", "_", -1), "-", "_", -1)
-		fmt.Printf("\trt_field = %s\n", attr)
-		switch rdfsRange {
-		case "xsd:string":
-			fmt.Printf("\trt_attr_string = %s_attr\n", attr)
-		case "xsd:dateTime":
-			fmt.Printf("\trt_attr_bigint = %s_attr\n", attr)
-		default:
-			log.Printf("@UNKNOWN RDFS:RANGE %v\n", rdfsRange)
+	data := ""
+	count := 0
+
+	for {
+		time.Sleep(300 * time.Millisecond)
+
+		main_queue.reopen_reader()
+
+		//log.Printf("@start prepare batch, count=%d", count)
+		for true {
+			data = main_cs.pop()
+			if data == "" {
+				break
+			}
+
+			tmp := MsgpackToIndividual(data)
+			if tmp == nil {
+				log.Println("@ERR GET_INDIVIDUAL: DECODING INDIVIDUAL")
+				continue
+			}
+
+			individual := MsgpackToIndividual(tmp.Resources["new_state"][0].StrData)
+
+			main_cs.commit_and_next(false)
+			count++
+
+			rdfType := individual.Resources["rdf:type"][0].StrData
+
+			_, ok := onto.individuals[rdfType]
+			if ok {
+				hashStr := md5.Sum([]byte(strings.Replace(strings.Replace(rdfType, ":", "_", -1), "-", "_", -1)))
+				//  	INSERT INTO e66c69899d879f63e8ae29efa74f10df (id, uri, uri_attr) VALUES (2, 'avc', 'avc');
+
+				query := fmt.Sprintf("SELECT MAX(id) FROM i%x;", hashStr)
+				rows, err := dbConn.Query(query)
+				if err != nil && err != sql.ErrNoRows {
+					log.Printf("@ERR ON SELECTING MAX ID FOR i%x(%s): %v\n", hashStr, rdfType, err)
+					log.Fatal("\t ", query)
+					continue
+				}
+
+				maxId := 0
+				if rows.Next() {
+					err = rows.Scan(&maxId)
+					if err != nil {
+						log.Fatalln("@ERR READING MAX ID: ", err)
+						continue
+					}
+					maxId++
+				}
+
+				query = fmt.Sprintf("INSERT INTO i%x (id, uri, uri_attr) VALUES (%d, '%v', '%v')", hashStr, maxId, individual.Uri,
+					individual.Uri)
+				_, err = dbConn.Exec(query)
+				if err != nil {
+					log.Println("@ERR ON EXECUTING QUERY: ", err)
+					log.Fatal("\t ", query)
+				}
+
+				log.Println(query)
+			} else {
+				log.Println("@UNKNOWN RDF:TYPE ", rdfType)
+			}
 		}
+
+		main_cs.sync()
 	}
-
-	fmt.Println("}")
-
-	/*for k, i := range onto.individuals {
-		if i.Resources["rdf:type"][0].StrData != "v-s:Person" {
-			continue
-		}
-		log.Println(k)
-		fmt.Printf("index %s {\n", strings.Replace(i.Resources["rdf:type"][0].StrData, ":", "_", -1))
-		fmt.Printf("\ttype=rt\n")
-		fmt.Printf("\tpath=data/sphinx-indexes/%s\n", strings.Replace(i.Resources["rdf:type"][0].StrData, ":", "_", -1))
-		fmt.Printf("%s", configCommon)
-		break
-	}*/
 }
