24011a4f814218932433d252e960d94934e36a65
Itiu <ValeriyBushenev@gmail.com>
2017-08-30 18:43:38 +0300

    (2)up to f0e8450d395c1a730338ce68b628222d9504c27c master

--- public/js/common/util.js
+++ public/js/common/util.js
@@ -1130,10 +1130,22 @@ function addRight(ticket, rights, subj_uri, obj_uri, new_uri) {
 
   if (subj_uri == undefined || obj_uri == undefined) {
     var error = new Error();
-    print("ERR! addRight: INVALID ARGS IN");
-    print("subj_uri=", subj_uri);
-    print("obj_uri=", obj_uri);
-    print("Error stack:", error.stack);
+
+    if (typeof window === "undefined")
+    {
+	print("ERR! addRight: INVALID ARGS IN");
+	print("subj_uri=", subj_uri);
+	print("obj_uri=", obj_uri);
+	print("Error stack:", error.stack);
+    }
+    else
+    {
+	console.log("ERR! addRight: INVALID ARGS IN");
+	console.log("subj_uri=", subj_uri);
+	console.log("obj_uri=", obj_uri);
+	console.log("Error stack:", error.stack);
+    }
+
     return;
   }
 
@@ -1145,15 +1157,35 @@ function addRight(ticket, rights, subj_uri, obj_uri, new_uri) {
     try {
       var prev = get_individual(ticket, new_uri);
       if (prev) {
-        if ( getUri(prev["rdf:type"]) !== "v-s:PermissionStatement" ) {
+        if ( getUri(prev["rdf:type"]) !== "v-s:PermissionStatement" ) 
+	{
           var error = new Error();
-          print ("ERR! addRight: INDIVIDUAL ALREADY EXISTS AND ITS TYPE IS NOT v-s:PermissionStatement, URI=" + new_uri);
-          print("Error stack:", error.stack);
+
+	  if (typeof window === "undefined")
+	  {
+            print ("ERR! addRight: INDIVIDUAL ALREADY EXISTS AND ITS TYPE IS NOT v-s:PermissionStatement, URI=" + new_uri);
+            print("Error stack:", error.stack);
+	  }
+	  else
+	  {
+            console.log ("ERR! addRight: INDIVIDUAL ALREADY EXISTS AND ITS TYPE IS NOT v-s:PermissionStatement, URI=" + new_uri);
+            console.log("Error stack:", error.stack);
+	  }
+
           return;
         }
       }
-    } catch (e) {
-      print("Error stack:", e.stack);
+    } catch (ex) {
+
+	  if (typeof window === "undefined")
+	  {
+            print("addRight:Error stack:", ex.stack);
+	  }
+	  else
+	  {
+            console.log("addRight:Error stack:", ex.stack);
+	  }
+
     }
   }
 
@@ -1223,3 +1255,4 @@ function clone(obj)
 
   throw new Error("Unable to copy obj! Its type isn't supported.");
 }
+
