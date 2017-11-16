7bd37dd6c927a2c021fe47623d37f6dba7c10169
Vadim <muller95@yandex.ru>
2017-11-16 11:32:54 +0300

    added connector files

---
+++ source/veda/connector/requestresponse.d
@@ -0,0 +1,14 @@
+module veda.connector.requestresponse;
+
+private
+{
+    import veda.core.common.context;
+}
+
+class RequestResponse
+{
+    ResultCode   common_rc;
+    ResultCode[] op_rc;
+    string[]     msgpacks;
+    ubyte[]      rights;
+}
