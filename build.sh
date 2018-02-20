1cf4fe53889402b126a614b1e94bab24702442c6
Itiu <ValeriyBushenev@gmail.com>
2018-02-20 19:49:41 +0300

    build: ren: gowebserver -> webserver

--- build.sh
+++ build.sh
@@ -32,9 +32,9 @@ if [ -z $1 ] || [ $1 == "bootstrap" ] || [ $1 == "veda" ] ; then
     rename "s/veda-bootstrap/veda/g" *
 fi
 
-if [ -z $1 ] || [ $1 == "webserver" ] || [ $1 == "veda-webserver" ] ; then
-    ./build-component.sh veda-webserver webserver
-fi
+#if [ -z $1 ] || [ $1 == "webserver" ] || [ $1 == "veda-webserver" ] ; then
+#    ./build-component.sh veda-webserver webserver
+#fi
 
 if [ -z $1 ] || [ $1 == "mstorage" ] || [ $1 == "veda-mstorage" ] ; then
     ./build-component.sh veda-mstorage mstorage
@@ -90,7 +90,7 @@ if [ -z $1 ] || [ $1 == "gowebserver" ] || [ $1 == "veda-gowebserver" ]; then
     go build
     cd ..
     cd ..
-    cp source/veda-gowebserver/veda-gowebserver ./
+    cp source/veda-gowebserver/veda-gowebserver ./veda-webserver    
 fi
 
 
