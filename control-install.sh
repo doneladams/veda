b2c52a102616e178695e11241c7ba764037544b2
Itiu <ValeriyBushenev@gmail.com>
2018-10-17 20:45:34 +0300

    *control-install.sh

--- control-install.sh
+++ control-install.sh
@@ -41,7 +41,7 @@ version=3.12
 build=3
 mkdir ~/temp
 cd ~/temp
-wget https://cmake.org/files/v$version/cmake-$version.$build-Linux-x86_64.sh.
+wget https://cmake.org/files/v$version/cmake-$version.$build-Linux-x86_64.sh
 sudo mkdir /opt/cmake
 sudo sh cmake-$version.$build-Linux-x86_64.sh --prefix=/opt/cmake --skip-license
 sudo ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
